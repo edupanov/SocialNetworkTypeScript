92b58211ce543e5fbc49b7554bca032f0fcc6e0b
edupanov <edupanov@gmail.com>
2020-08-24 21:57:51 +0300

    add some components

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import style from './Profile.module.css';
+import style from "./Profile.module.css";
 import MyPosts from "./MyPosts/MyPosts";
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
+
 
 const Profile = () => {
     return (
--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import style from './ProfileInfo.module.css';
+import style from "./ProfileInfo.module.css";
 
 const ProfileInfo = () => {
     return (
         <div>
 
             <div>
-                <img src='https://www.w3schools.com/howto/img_snow_wide.jpg'/>
+                <img src="https://www.w3schools.com/howto/img_snow_wide.jpg"/>
             </div>
             <div className={style.descriptionBlock}>
                 1
