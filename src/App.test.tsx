5fda4f444ea665082d1fb962d34b4e4f54a0f72b
edupanov <edupanov@gmail.com>
2020-09-05 11:57:01 +0300

    test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import state, {addPost, updateNewPostText} from "./redux/state";
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
+  const { getByText } = render(<App
+      state={state}
+      addPost={addPost}
+      updateNewPostText={updateNewPostText}
+  />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
